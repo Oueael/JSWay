8428d3b70996205a9e066e5718d6701388fe68bd
Oueael <125812553+Oueael@users.noreply.github.com>
2023-10-20 20:46:24 -0400

    Update chapter06_answers.js

--- chapter06_answers.js
+++ chapter06_answers.js
@@ -1,50 +1,48 @@
-// const aurora = {
-//     name: "Aurora",
-//     health: 150,
-//     strength: 25,
-//     xp: 0,  // Corrected from xp = 0 to xp: 0
-//     describe: function() {
-//         return `${this.name} has ${this.health}, ${this.strength} strength, and ${this.xp} XP points`;
-//     }
-// }
+const aurora = {
+    name: "Aurora",
+    health: 150,
+    strength: 25,
+    xp: 0,
+    describe: function() {
+        return `${this.name} has ${this.health}, ${this.strength} strength, and ${this.xp} XP points`;
+    }
+};
 
-// // Aurora is harmed by an arrow
-// aurora.health -= 20;
+// Aurora is harmed by an arrow
+aurora.health -= 20;
 
-// // Aurora equips a strength necklace
-// aurora.strength += 10;
+// Aurora equips a strength necklace
+aurora.strength += 10;
 
-// // Aurora learns a new skill
-// aurora.xp += 15;
+// Aurora learns a new skill
+aurora.xp += 15;
 
-// console.log(aurora.describe());
+console.log(aurora.describe());
 
-// TODO: create the dog object here
+const dog = {
+    name:"Fang",
+    species:"boarhound",
+    size:75,
+    bark:function(){
+        return "Grrr! Grrr!"
+    }
+};
 
-// const dog = {
-//     name:"Fang",
-//     species:"boarhound",
-//     size:75,
-//     bark:function(){
-//         return "Grrr! Grrr!"
-//     }
-// }
+console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
 
-// console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
-// console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
+const r = Number(prompt("Enter the circle radius:"));
+const circle = {
+    circumference:function(){
+        return Math.PI * 2 * r
+    },
+    area:function(){
+        return Math.PI * r * r 
+    }
+};
 
-// const r = Number(prompt("Enter the circle radius:"));
-// const circle = {
-//     circumference:function(){
-//         return Math.PI * 2 * r
-//     },
-//     area:function(){
-//         return Math.PI * r *r 
-//     }
-// }
-
-// console.log(`Its circumference is ${circle.circumference()}`);
-// console.log(`Its area is ${circle.area()}`);
+console.log(`Its circumference is ${circle.circumference()}`);
+console.log(`Its area is ${circle.area()}`);
 
 const account = {
     name: "Alex",
@@ -57,14 +55,7 @@ const account = {
     }
 };
 
-// Show the initial account description
 console.log(account.describe());
-
-// Credit 250 to the account
 account.credit(250);
-
-// Debit 80 from the account (credit a negative value)
 account.credit(-80);
-
-// Show the account description again
 console.log(account.describe());
